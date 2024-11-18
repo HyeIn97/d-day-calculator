c5c3caccbf62fc4f6089b328e1d0870517e083fc
HyeIn97 <wslx97@naver.com>
2024-11-18 23:28:08 +0900

    [refactor] theme을 이용해 statusbar 색상을 변경한다.

--- app/src/main/java/com/example/d_day_calculator/SplashActivity.kt
+++ app/src/main/java/com/example/d_day_calculator/SplashActivity.kt
@@ -1,28 +1,23 @@
 package com.example.d_day_calculator
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowInsetsControllerCompat
+import java.util.Timer
+import kotlin.concurrent.schedule
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private val timer = Timer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_splash)
-
-        window.apply {
-            statusBarColor = this@SplashActivity.getColor(R.color.citrineWhite)
-            WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars = true
-            setDecorFitsSystemWindows(false)
-            insetsController?.hide(WindowInsets.Type.navigationBars())
-        }
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if (!hasFocus) return
-        window.apply {
-            setDecorFitsSystemWindows(false)
-            insetsController?.hide(WindowInsets.Type.navigationBars())
+        timer.schedule(2000) {
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
         }
     }
 }
