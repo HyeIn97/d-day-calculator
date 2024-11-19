ae8e6d3fce0f1421d63ea82117c0e1f7554818c6
HyeIn97 <wslx97@naver.com>
2024-11-19 22:55:03 +0900

    [refactor] ui 디렉토리를 변경한다.

---
+++ app/src/main/java/com/example/d_day_calculator/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.d_day_calculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.example.d_day_calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this), null, false)
+        setContentView(binding.root)
+
+        initListener()
+    }
+
+    private fun initListener() = with(binding) {
+        addBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, DayActivity::class.java))
+        }
+    }
+}
