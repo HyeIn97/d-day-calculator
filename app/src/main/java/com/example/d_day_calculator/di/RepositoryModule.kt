277aa0d8d08c62107cb09e04c9e3af238b89f1e6
HyeIn97 <wslx97@naver.com>
2024-12-10 22:14:30 +0900

    [feat] di 모듈을 생성한다.

---
+++ app/src/main/java/com/example/d_day_calculator/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package com.example.d_day_calculator.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+}
