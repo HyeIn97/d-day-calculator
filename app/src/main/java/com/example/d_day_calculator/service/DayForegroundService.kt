900831daa0eddf5f477f3ead9631e7b7f8305d19
HyeIn97 <wslx97@naver.com>
2024-12-16 17:32:39 +0900

    [feat] 서비스 권한을 추가한다.

--- app/src/main/java/com/example/d_day_calculator/service/DayForegroundService.kt
+++ app/src/main/java/com/example/d_day_calculator/service/DayForegroundService.kt
@@ -1,14 +1,30 @@
 package com.example.d_day_calculator.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.d_day_calculator.R
 
 class DayForegroundService : Service() {
+    private val CHANEL_ID = "1001"
+    private val CHANEL_NAME = "DAY_CHANEL"
+    private var notificationManager: NotificationManager? = null
+    private var notificationChannel: NotificationChannel? = null
+    private var notificationBuilder: NotificationCompat.Builder? = null
+
     override fun onBind(p0: Intent?): IBinder? = null
 
     override fun onCreate() {
         super.onCreate()
+
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationChannel =
+            NotificationChannel(CHANEL_ID, CHANEL_NAME, NotificationManager.IMPORTANCE_NONE)
+        notificationBuilder = NotificationCompat.Builder(this@DayForegroundService, CHANEL_ID)
+            .setSmallIcon(R.drawable.ic_d)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
