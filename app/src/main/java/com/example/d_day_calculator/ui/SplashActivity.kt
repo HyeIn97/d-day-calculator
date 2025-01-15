2965bf5284545915b90635159d18f1df79367199
HyeIn97 <wslx97@naver.com>
2025-01-15 22:06:00 +0900

    [feat] 알림 퍼미션 권한 요청 로직을 추가한다.

--- app/src/main/java/com/example/d_day_calculator/ui/SplashActivity.kt
+++ app/src/main/java/com/example/d_day_calculator/ui/SplashActivity.kt
@@ -1,9 +1,15 @@
 package com.example.d_day_calculator.ui
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.d_day_calculator.R
 import java.util.Timer
 import kotlin.concurrent.schedule
@@ -14,11 +20,34 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_splash)
+
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                goMainActivity()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            goMainActivity()
+        } else {
+            Toast.makeText(this, getString(R.string.permission_toast), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun goMainActivity() {
         timer.schedule(2000) {
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }
     }
 }
+
