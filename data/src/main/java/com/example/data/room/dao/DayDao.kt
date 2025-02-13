f98a095c3610adf36d48d6165b223cc1104ec520
HyeIn97 <wslx97@naver.com>
2025-02-13 10:43:00 +0900

    [feat] 수정, 삭제를 위한 쿼리 추가한다.

--- data/src/main/java/com/example/data/room/dao/DayDao.kt
+++ data/src/main/java/com/example/data/room/dao/DayDao.kt
@@ -3,6 +3,7 @@ package com.example.data.room.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.data.room.entity.DayEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -12,5 +13,11 @@ interface DayDao {
     fun insertDay(day: DayEntity): Long
 
     @Query("SELECT * FROM days ORDER BY `key` DESC")
-     fun getAllDay(): Flow<List<DayEntity>>
+    fun getAllDay(): Flow<List<DayEntity>>
+
+    @Query("DELETE FROM days WHERE title = :title")
+    fun deleteDay(title: String)
+
+    @Update
+    fun updateDay(day: DayEntity)
 }
