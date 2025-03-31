772083579367278ef53cc103dd22ad5888c087fd
HyeIn97 <wslx97@naver.com>
2025-03-31 20:37:34 +0900

    [refactor] service notify 생성 로직을 수정한다.

--- presentation/src/main/java/com/example/presentation/service/DayForegroundService.kt
+++ presentation/src/main/java/com/example/presentation/service/DayForegroundService.kt
@@ -4,9 +4,11 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
@@ -29,11 +31,8 @@ class DayForegroundService : Service() {
     @Inject
     lateinit var getNotificationDay: GetNotificationDayUseCase
 
-    private var CHANEL_ID = 10001
-    private val NOTIFICATION_NAME = "DAY_CHANEL"
-    private var notificationManager: NotificationManager? = null
-    private var notificationChannel: NotificationChannel? = null
-    private var notificationBuilder: NotificationCompat.Builder? = null
+    private val CHANEL_ID = "10001"
+    private val CHANEL_NAME = "DAY_CHANEL"
     private val job = CoroutineScope(Dispatchers.IO)
 
     override fun onBind(p0: Intent?): IBinder? = null
@@ -69,25 +68,29 @@ class DayForegroundService : Service() {
             set(Calendar.MINUTE, 0)
             set(Calendar.SECOND, 0)
         }.time.time
-
         day = ((today - endDay!!) / (60 * 60 * 24 * 1000)).toInt()
 
         val dayTxt = if (day > 0) "D+$day" else if (day == 0) "D+0" else "D" + day.toString()
 
-        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationChannel = NotificationChannel(item.key.toString(), NOTIFICATION_NAME, NotificationManager.IMPORTANCE_HIGH)
-        notificationBuilder = NotificationCompat.Builder(this@DayForegroundService, item.key.toString())
+        val notification = NotificationCompat.Builder(this@DayForegroundService, CHANEL_ID)
+            .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_large_push))
             .setSmallIcon(R.drawable.ic_app)
             .setContentTitle(dayTxt)
             .setContentText(item.title)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setColor(ContextCompat.getColor(this@DayForegroundService, R.color.black))
+            .setOngoing(true)
+            .setShowWhen(false)
             .setAutoCancel(false)
 
-        notificationChannel?.let {
-            notificationManager?.createNotificationChannel(it)
+        val channel = NotificationChannel(CHANEL_ID, CHANEL_NAME, IMPORTANCE_HIGH).apply {
+            setShowBadge(false)
         }
 
-        startForeground(CHANEL_ID, notificationBuilder?.build())
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+
+        startForeground(item.key, notification.build())
     }
 
     private fun getDays() = job.launch {
