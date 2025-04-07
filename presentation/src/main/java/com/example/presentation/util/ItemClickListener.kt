72efb9215c6a8562b25928829c06f5bd2b6514b3
HyeIn97 <wslx97@naver.com>
2025-04-07 22:51:51 +0900

    [refactor] 포지션으로 어뎁터를 갱신하기 위해 리스너를 수정한다.

--- presentation/src/main/java/com/example/presentation/util/ItemClickListener.kt
+++ presentation/src/main/java/com/example/presentation/util/ItemClickListener.kt
@@ -1,6 +1,6 @@
 package com.example.presentation.util
 
 interface ItemClickListener<T> {
-    fun itemSettingClick(data: T) {}
+    fun itemSettingClick(position: Int, data: T) {}
     fun itemDeleteClick(data: T) {}
 }
